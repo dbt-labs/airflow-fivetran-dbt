2783aaed4dc06e80f7fd2e039a118dbbe959b5ff
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-01-15 10:20:09 -0700

    first commit

---
+++ example_fivetrain_dbt_run.py
@@ -0,0 +1,61 @@
+
+from datetime import timedelta
+from pprint import pprint
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import DAG
+
+# Operators; we need this to operate!
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+# These args will get passed on to each operator
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['airflow@fishtownanalytics.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
+
+args = {
+    'owner': 'airflow',
+}
+
+dag = DAG(
+    dag_id='example_python_operator',
+    default_args=args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=['example'],
+)
+
+def fivetran_exract(ds, **kwargs):
+    """Print the Airflow context and ds variable from the context."""
+    pprint(kwargs)
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+
+run_fivetran_extract = PythonOperator(
+    task_id='extract_pokemon_data',
+    python_callable=fivetran_exract,
+    dag=dag,
+)
+
+run_fivetran_extract
