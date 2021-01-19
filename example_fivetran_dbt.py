a8b19cf30522797968e272ebbd96619d4fc8bd5d
Matt Winkler <matt.winkler@fishtownanalytics.com>
2021-01-19 08:54:24 -0700

    abstract fivetran api

--- example_fivetran_dbt.py
+++ example_fivetran_dbt.py
@@ -3,7 +3,7 @@ import json
 import os
 import requests
 
-from datetime import timedelta
+from datetime import datetime, timedelta
 from pprint import pprint
 
 # The DAG object; we'll need this to instantiate a DAG
@@ -12,11 +12,16 @@ from airflow import DAG
 # Operators; we need this to operate!
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
+
+from fivetran import FivetranApi
+
 # These args will get passed on to each operator
 # You can override them on a per-task basis during operator initialization
 
 FIVETRAN_API_KEY = os.getenv('FIVETRAN_API_KEY', '')
-BASE_URL = 'https://api.fivetran.com/v1/'
+DBT_API_KEY = os.getenv('DBT_API_KEY', '')
+
+ft = FivetranApi(api_token=FIVETRAN_API_KEY)
 
 default_args = {
     'owner': 'airflow',
@@ -43,6 +48,7 @@ default_args = {
 
 args = {
     'owner': 'airflow',
+    'start_date': datetime.now()
 }
 
 dag = DAG(
@@ -53,32 +59,65 @@ dag = DAG(
     tags=['example'],
 )
 
-def fivetran_connector_sync(ds, base_url=BASE_URL, api_route='connectors', api_key=FIVETRAN_API_KEY, **kwargs):
-    """Print the Airflow context and ds variable from the context."""
+# def fivetran_connector_sync(ds, base_url=BASE_URL, api_route='connectors', api_key=FIVETRAN_API_KEY, **kwargs):
+#     """Print the Airflow context and ds variable from the context."""
 
-    #pprint(kwargs)
-    #print(ds)
-    #return 'Whatever you return gets printed in the logs'
-    url = base_url + api_route + '/' + 'warn_enormously' + '/' + 'force'
-    headers = {'Content-Type': 'application/json', 'Authorization': f'Basic {api_key}' }
-    data = {} # this endpoint takes an empty payload
-    response = requests.post(url, json=data, headers=headers)
+#     #pprint(kwargs)
+#     #print(ds)
+#     #return 'Whatever you return gets printed in the logs'
+#     connector_id = kwargs['dag_run'].conf['connector_id'] #warn_enormously
 
-    if response.status_code == 200:
-        return json.loads(response.content)
+#     url = base_url + api_route + '/' + connector_id + '/' + 'force'
+#     headers = {'Content-Type': 'application/json', 'Authorization': f'Basic {api_key}' }
+#     data = {} # this endpoint takes an empty payload
+#     response = requests.post(url, json=data, headers=headers)
+
+#     if response.status_code == 200:
+#         return json.loads(response.content)
     
-    else:
-        raise RuntimeError(response.text)
-
-# start sync (above)
-# check sync status
-# start dbt run
-# check run status
+#     else:
+#         raise RuntimeError(response.text)
 
 run_fivetran_connector_sync = PythonOperator(
-    task_id='extract_pokemon_data',
-    python_callable=fivetran_connector_sync,
+    task_id='start_data_sync',
+    python_callable=ft.force_connector_sync,
     dag=dag,
 )
 
-run_fivetran_connector_sync
+def check_connector_sync_status(ds, **kwargs):
+    return 'checking connector sync status'
+
+run_check_connector_sync_status = PythonOperator(
+    task_id='extract_pokemon_data',
+    python_callable=check_connector_sync_status,
+    dag=dag,
+)
+
+def dbt_job_run(ds, **kwargs):
+    return 'Running dbt jobx'
+
+run_dbt_job_run = PythonOperator(
+    task_id='dbt_job_run',
+    python_callable=dbt_job_run,
+    dag=dag,
+)
+
+def check_dbt_job_run_status(ds, **kwargs):
+    return 'checking dbt job run status'
+
+run_check_dbt_job_run_status = PythonOperator(
+    task_id='check_dbt_job_run_status',
+    python_callable=check_dbt_job_run_status,
+    dag=dag,
+)
+
+run_fivetran_connector_sync >> check_connector_sync_status >> run_dbt_job_run >> check_dbt_job_run_status
+
+# start sync (above)
+# poll to check connector status - GET connector data
+## verify whether last succeeded date is after the trigger date
+## if so, start dbt run
+
+# start dbt run
+
+# check run status
---
+++ fivetran.py
@@ -0,0 +1,85 @@
+
+# -*- coding: utf-8 -*-
+import json
+import requests
+import time
+
+class FivetranApi(object):
+    """
+    Class for interacting with the Fivetran API
+    * :py:meth:`list_jobs` - list all Jobs for the specified Account ID
+    * :py:meth:`get_run` - Get information about a specified Run ID
+    * :py:meth:`trigger_job_run` - Trigger a Run for a specified Job ID
+    * :py:meth: `try_get_run` - Attempts to get information about a specific Run ID for up to max_tries
+    * :py:meth: `run_job` - Triggers a run for a job using the job name
+    """
+
+    def __init__(self, api_token):
+        self.api_token = api_token
+        self.api_base = 'https://api.fivetran.com/v1/'
+
+    def _get(self, url_suffix):
+        url = self.api_base + url_suffix
+        headers = {'Authorization': 'Token %s' % self.api_token}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            raise RuntimeError(response.content)
+
+    def _post(self, url_suffix, data=None):
+        url = self.api_base + url_suffix
+        print('request url: ', url)
+        print('showing request body: ', json.dumps(data))
+        headers = {'Content-Type': 'application/json', 'Authorization': 'token %s' % self.api_token}
+
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+        
+        if response.status_code == 200:
+            return json.loads(response.content)
+        else:
+            raise RuntimeError(response.text)
+    
+    def get_groups(self):
+        """Returns group information from the fivetran account"""
+        return self._get(url_suffix='groups/').get('data')
+
+    def get_group_connectors(self, group_id):
+        """Returns information about connectors attached to a group"""
+        return self._get(url_suffix=f'groups/{group_id}/connectors').get('data')
+    
+    def get_connector(self, connector_id):
+        """Returns information about the connector under connector_id"""
+        return self._get(url_suffix=f'connectors/{connector_id}').get('data')
+    
+    def force_connector_sync(self, connector_id, request_body={}, **kwargs):
+        """Triggers a run of the target connector under connector_id"""
+        connector_id = kwargs['dag_run'].conf['connector_id']
+        return self._post(url_suffix=f'connectors/{connector_id}/force', data=request_body).get('data')
+
+    # def list_jobs(self):
+    #     return self._get('/accounts/%s/jobs/' % self.account_id).get('data')
+
+    # def get_run(self, run_id):
+    #     return self._get('/accounts/%s/runs/%s/' % (self.account_id, run_id)).get('data')
+
+    # def trigger_job_run(self, job_id, data=None):
+
+    #     return self._post(url_suffix='/accounts/%s/jobs/%s/run/' % (self.account_id, job_id), data=data).get('data')
+
+    # def try_get_run(self, run_id, max_tries=3):
+    #     for i in range(max_tries):
+    #         try:
+    #             run = self.get_run(run_id)
+    #             return run
+    #         except RuntimeError as e:
+    #             print("Encountered a runtime error while fetching status for {}".format(run_id))
+    #             time.sleep(10)
+
+    #     raise RuntimeError("Too many failures ({}) while querying for run status".format(run_id))
+
+    # def create_job(self, data=None):
+    #     return self._post(url_suffix='/accounts/%s/jobs/' % (self.account_id), data=data)
+
+    # def update_job(self, job_id, data=None):
+    #     return self._post(url_suffix='/accounts/%s/jobs/%s/' % (self.account_id, job_id), data=data)
